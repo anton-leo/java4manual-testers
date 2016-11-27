9ab905a530fb780492686029b77d2c9878380893
anton-leo <spilornis@gmail.com>
2016-11-27 20:50:11 +0300

    Change to adhere naming convention

--- src/main/java/personal/edu/MortgageMain.java
+++ src/main/java/personal/edu/MortgageMain.java
@@ -3,28 +3,27 @@ package personal.edu;
 public class MortgageMain {
 
     public static void main(String[] args) {
-
         int counter = 0;
-        double minpayment = 57000;
+        double minPayment = 57000;
         double remainder = 3_600_000.0;
         double percentage = 12.9;
-        double deltasum = 0.0;
-        while (remainder > minpayment) {
-            remainder = (remainder - minpayment);
+        double deltaSum = 0.0;
+        while (remainder > minPayment) {
+            remainder = (remainder - minPayment);
             double delta = (remainder / 100 * percentage / 12);
             remainder = remainder + delta;
             counter++;
             if (counter == 1) {
-                deltasum = delta;
+                deltaSum = delta;
             } else {
-                deltasum = deltasum + delta;
+                deltaSum = deltaSum + delta;
             }
         }
         System.out.print("You will be able to repay your apartment in ");
         System.out.print(counter + 1);
         System.out.println(" months");
         System.out.print("And you pay to the bank ");
-        System.out.println(deltasum);
+        System.out.println(deltaSum);
     }
 }
 
